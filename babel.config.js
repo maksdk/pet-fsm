e716d3c244846dbd0fed7187489369436dc5aec2
maksdk <havryshchenko@gmail.com>
2020-10-10 08:23:35 +0300

    Added babel / webpack / ts tasks

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 // @ts-check
 module.exports = {
     presets: [
-        [ '@babel/preset-env', { targets: { node: 'current' } } ],
-        '@babel/preset-typescript',
+        ['@babel/preset-env', {
+            targets: {
+                node: 'current'
+            }
+        }],
+        '@babel/preset-typescript'
+    ],
+    plugins: [
+        "@babel/plugin-proposal-class-properties"
     ]
 };
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }],
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
