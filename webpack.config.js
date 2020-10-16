f8a23ed95df620f1c485cc5a861f072f92e7f563
maksdk <havryshchenko@gmail.com>
2020-10-16 06:54:21 +0300

    Rebuilt project

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'index.js'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
